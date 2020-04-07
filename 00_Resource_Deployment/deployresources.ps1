775cbb7479bf27cd2c43570bd38b811d088e5aa9
Hallie <hallieylam@gmail.com>
2020-04-07 15:58:25 -0600

    updating deployment scripts

--- 00_Resource_Deployment/deployresources.ps1
+++ 00_Resource_Deployment/deployresources.ps1
@@ -1,6 +1,7 @@
 $subscriptionID= '2851f2d7-fcd7-480d-b13d-b91fe8316aae'
-$resourceGroupName = 'Digital_Shipping_Blockchain'
-$resourceGroupLocation= 'SouthCentralUS'
+$resourceGroupName = 'Shipping_Blockchain'
+$resourceGroupLocation= 'SoutheastAsia'
+$cmsbcLocation = 'JapanEast'
 
 az login
 
@@ -15,10 +16,16 @@ az group deployment create `
    --name "deployment"  `
    --resource-group $resourceGroupName `
    --template-file cmsbcdeploy.json `
-   --parameters cmsbcdeploy.parameters.json
+   --parameters cmsbcdeploy.parameters.json `
+   --parameters cosmosLocation=${cmsbcLocation} bcLocation=${cmsbcLocation}
 
 az group deployment create `
    --name "deploymentfunction" `
    --resource-group $resourceGroupName `
    --template-file fxappdeploy.json `
-   --parameters Join-Path fxappdeploy.parameters.json
+   --parameters fxappdeploy.parameters.json
+
+
+
+Write-Host -NoNewLine 'Your deployment has finished, press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
