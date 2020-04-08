d38a9882d534acdb1c870470e770c1af71a71a70
Hallie <hallieylam@gmail.com>
2020-04-08 13:10:55 -0600

    restructured az deployment

---
+++ azureResourceDeployment/deployresources.ps1
@@ -0,0 +1,46 @@
+$subscriptionID= '036cb61a-ccf5-49ea-90fc-391eb7720410'
+$resourceGroupName = 'Digital_Shipping_Blockchain'
+$resourceGroupLocation= 'KoreaSouth'
+
+#these values can be used to override resource parameters specified in template
+$cmsLocation = 'KoreaSouth'
+$bcLocation = 'JapanEast'
+
+az login
+
+az account set --subscription $subscriptionID
+
+#create resource group
+az group create `
+    --location $resourceGroupLocation `
+    --name $resourceGroupName `
+    --subscription $subscriptionID
+
+#create cosmos account
+az group deployment create `
+   --name "cmsdeployment"  `
+   --resource-group $resourceGroupName `
+   --template-file cosmosdeploy.json `
+   --parameters cosmosdeploy.parameters.json `
+   --parameters cmsLocation=${cmsLocation}
+
+#create blockchain account
+az group deployment create `
+   --name "bcdeployment"  `
+   --resource-group $resourceGroupName `
+   --template-file blockchaindeploy.json `
+   --parameters blockchaindeploy.parameters.json `
+   --parameters bcLocation=${bcLocation}
+
+#create function app with serivce plan and and storage account
+#location is the same as resource group
+az group deployment create `
+   --name "fxdeployment" `
+   --resource-group $resourceGroupName `
+   --template-file fxappdeploy.json `
+   --parameters fxappdeploy.parameters.json
+
+
+
+Write-Host -NoNewLine 'Your deployment has finished, press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
